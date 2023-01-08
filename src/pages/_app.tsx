aa4c568823db997ad9dc863f7160c2cc6cf762ee
jhigger <njohnjhigger@gmail.com>
2023-01-08 12:15:38 +0800

    coming soon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import "../styles/globals.css";
 import type { AppType } from "next/dist/shared/lib/utils";
 import Navbar from "../components/Navbar";
+import { useRouter } from "next/router";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+	const router = useRouter();
 	return (
 		<>
-			<Navbar />
+			{router.pathname !== "/comingsoon" && <Navbar />}
 			<Component {...pageProps} />
 		</>
 	);
---
+++ src/pages/comingsoon.tsx
@@ -0,0 +1,26 @@
+const ComingSoonPage = () => {
+	return (
+		<main className="relative h-screen w-screen">
+			<video
+				className="h-full w-full object-cover object-center"
+				width="100%"
+				height="100%"
+				autoPlay
+				loop
+				muted
+				playsInline
+			>
+				<source src="/assets/imagine480.webm" type="video/webm" />
+			</video>
+			<div className="absolute top-0 left-0 h-full w-full bg-gray-900 bg-opacity-5 bg-clip-padding backdrop-blur-md backdrop-filter">
+				<div className="flex h-full flex-col items-center justify-center p-4">
+					<h1 className="py-4 text-center font-montserrat text-6xl font-bold leading-5 text-white">
+						Coming Soon
+					</h1>
+				</div>
+			</div>
+		</main>
+	);
+};
+
+export default ComingSoonPage;
