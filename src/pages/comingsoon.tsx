cfe19a47534ac514a66b47560578dad5bace0da9
jhigger <njohnjhigger@gmail.com>
2023-01-08 13:48:12 +0800

    revert to 480

--- src/pages/comingsoon.tsx
+++ src/pages/comingsoon.tsx
@@ -10,10 +10,7 @@ const ComingSoonPage = () => {
 				muted
 				playsInline
 			>
-				<source
-					src="https://d20wbi5cgoftj2.cloudfront.net/qtopiavideos/imagine.mp4"
-					type="video/webm"
-				/>
+				<source src="/assets/imagine480.webm" type="video/webm" />
 			</video>
 			<div className="absolute top-0 left-0 h-full w-full bg-gray-900 bg-opacity-50 bg-clip-padding backdrop-blur-sm backdrop-filter">
 				<div className="flex h-full flex-col items-center justify-center p-4">
